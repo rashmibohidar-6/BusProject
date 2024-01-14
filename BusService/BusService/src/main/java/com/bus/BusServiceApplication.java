6a01298d7297e0276797081ed259245a7d1968d4
rashmibohidar-6 <bohidar.rashmi@gmail.com>
2024-01-15 00:22:46 +0530

    Initial Commit

--- BusService/BusService/src/main/java/com/bus/BusServiceApplication.java
+++ BusService/BusService/src/main/java/com/bus/BusServiceApplication.java
@@ -2,8 +2,10 @@ package com.bus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BusServiceApplication {
 
 	public static void main(String[] args) {
